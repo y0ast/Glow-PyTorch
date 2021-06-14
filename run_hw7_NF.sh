8f7fb628ef67ab89cf94492f3f4945d96aca3648
yelloooowww <cococing0324@gmail.com>
2021-06-14 21:38:11 +0800

    fix load pretrined weight err

--- run_hw7_NF.sh
+++ run_hw7_NF.sh
@@ -1,8 +1,12 @@
 LOG=$(date +%m%d_%H%M_logs)
 echo $LOG
+mkdir $LOG
 python3 train.py --y_condition --output_dir $LOG \
                   --batch_size 16 \
-                  --epochs 25 \
+                  --epochs 10 \
                   --dataroot "/home/yellow/deep-learning-and-practice/hw7/dataset/task_2/" \
-                  --K 5 \
-                  --L 3
+                  --K 6 \
+                  --L 3 \
+                  # --saved_model 0614_2101_logs/glow_checkpoint_937.pt \
+
+cp run_hw7_NF.sh $LOG/runs_hw7_NF.sh
